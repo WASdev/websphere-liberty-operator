3ead8fa328c38c443a13bc4cb1fcbe359c6f277c
Leo Christy Jesuraj <leojc@ca.ibm.com>
2022-05-13 18:28:51 -0400

    Check for service account's pull secret

--- controllers/webspherelibertyapplication_controller.go
+++ controllers/webspherelibertyapplication_controller.go
@@ -255,6 +255,13 @@ func (r *ReconcileWebSphereLiberty) Reconcile(ctx context.Context, request ctrl.
 			return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
 		}
 	}
+	
+	// Check if the ServiceAccount has a valid pull secret before creating the deployment/statefulset
+	// or setting up knative. Otherwise the pods can go into an ImagePullBackOff loop
+	saErr := oputils.ServiceAccountPullSecretExists(instance, r.GetClient())
+	if saErr != nil {
+		return r.ManageError(saErr, common.StatusConditionTypeReconciled, instance)
+	}
 
 	isKnativeSupported, err := r.IsGroupVersionSupported(servingv1.SchemeGroupVersion.String(), "Service")
 	if err != nil {
