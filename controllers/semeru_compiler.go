ba27285c22f3bca13e3ab71a788b10c9ab5d9ee7
Jason Yong <jason_yong@uk.ibm.com>
2022-10-18 14:07:50 +0100

    Updates to semeru service
    Signed-off-by: Jason Yong <jason_yong@uk.ibm.com>

--- controllers/semeru_compiler.go
+++ controllers/semeru_compiler.go
@@ -6,16 +6,22 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
+
+const (
+	semeruCompilerSuffix    = "-semeru-compiler"
+	semeruCompilerLabelName = "semeru-compiler"
+)
+
 // Create the Deployment and Service objects for a Semeru Compiler used by a Websphere Liberty Application
-func (r *ReconcileWebSphereLiberty) reconcileSemeruCompiler(la *wlv1.WebSphereLibertyApplication) (error, string) {
+func (r *ReconcileWebSphereLiberty) reconcileSemeruCompiler(wlva *wlv1.WebSphereLibertyApplication) (error, string) {
 	compilerMeta := metav1.ObjectMeta{
-		Name:      la.GetName() + `-semeru-compiler`,
-		Namespace: la.GetNamespace(),
+		Name:      wlva.GetName() + semeruCompilerSuffix,
+		Namespace: wlva.GetNamespace(),
 	}
 	// Create the Semeru Service object
 	semsvc := &corev1.Service{ObjectMeta: compilerMeta}
-	err := r.CreateOrUpdate(semsvc, la, func() error {
-		createSemeruService(semsvc, la)
+	err := r.CreateOrUpdate(semsvc, wlva, func() error {
+		reconcileSemeruService(semsvc, wlva)
 		return nil
 	})
 	if err != nil {
@@ -24,11 +30,11 @@ func (r *ReconcileWebSphereLiberty) reconcileSemeruCompiler(la *wlv1.WebSphereLi
 	return nil, ""
 }
 
-func createSemeruService(svc *corev1.Service, la *wlv1.WebSphereLibertyApplication) {
+func reconcileSemeruService(svc *corev1.Service, wlva *wlv1.WebSphereLibertyApplication) {
 	var port int32 = 38400
 	var timeout int32 = 86400
-	svc.Labels = getLabels(la)
-	svc.Spec.Selector = getSelectors(la)
+	svc.Labels = getLabels(wlva)
+	svc.Spec.Selector = getSelectors(wlva)
 	if len(svc.Spec.Ports) == 0 {
 		svc.Spec.Ports = append(svc.Spec.Ports, corev1.ServicePort{})
 	}
@@ -44,20 +50,21 @@ func createSemeruService(svc *corev1.Service, la *wlv1.WebSphereLibertyApplicati
 }
 
 // Create the Selector map for a Semeru Compiler
-func getSelectors(la *wlv1.WebSphereLibertyApplication) map[string]string {
+func getSelectors(wlva *wlv1.WebSphereLibertyApplication) map[string]string {
 	requiredSelector := make(map[string]string)
-	requiredSelector["app.kubernetes.io/component"] = `semeru-compiler`
-	requiredSelector["app.kubernetes.io/instance"] = la.GetName() + `-semeru-compiler`
-	requiredSelector["app.kubernetes.io/part-of"] = la.GetName()
+	requiredSelector["app.kubernetes.io/component"] = semeruCompilerLabelName
+	requiredSelector["app.kubernetes.io/instance"] = wlva.GetName() + semeruCompilerSuffix
+	requiredSelector["app.kubernetes.io/part-of"] = wlva.GetName()
 	return requiredSelector
 }
+
 // Create the Labels map for a Semeru Compiler
-func getLabels(la *wlv1.WebSphereLibertyApplication) map[string]string {
+func getLabels(wlva *wlv1.WebSphereLibertyApplication) map[string]string {
 	requiredLabels := make(map[string]string)
-	requiredLabels["app.kubernetes.io/name"] = la.GetName() + `-semeru-compiler`
-	requiredLabels["app.kubernetes.io/instance"] = la.GetName() + `-semeru-compiler`
+	requiredLabels["app.kubernetes.io/name"] = wlva.GetName() + semeruCompilerSuffix
+	requiredLabels["app.kubernetes.io/instance"] = wlva.GetName() + semeruCompilerSuffix
 	requiredLabels["app.kubernetes.io/managed-by"] = `websphere-liberty-operator`
-	requiredLabels["app.kubernetes.io/component"] = `semeru-compiler`
-	requiredLabels["app.kubernetes.io/part-of"] = la.GetName()
+	requiredLabels["app.kubernetes.io/component"] = semeruCompilerLabelName
+	requiredLabels["app.kubernetes.io/part-of"] = wlva.GetName()
 	return requiredLabels
 }
--- controllers/webspherelibertyapplication_controller.go
+++ controllers/webspherelibertyapplication_controller.go
@@ -258,7 +258,14 @@ func (r *ReconcileWebSphereLiberty) Reconcile(ctx context.Context, request ctrl.
 			return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
 		}
 	}
-
+	// Reconcile the Semeru Compiler deployment and service
+	message := "Start Semeru Compiler reconcile"
+	reqLogger.Info(message)
+	err, message = r.reconcileSemeruCompiler(instance)
+	if err != nil {
+		reqLogger.Error(err, message)
+		return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
+	}
 	// Check if the ServiceAccount has a valid pull secret before creating the deployment/statefulset
 	// or setting up knative. Otherwise the pods can go into an ImagePullBackOff loop
 	saErr := oputils.ServiceAccountPullSecretExists(instance, r.GetClient())
@@ -609,15 +616,7 @@ func (r *ReconcileWebSphereLiberty) Reconcile(ctx context.Context, request ctrl.
 	} else {
 		reqLogger.V(1).Info(fmt.Sprintf("%s is not supported", prometheusv1.SchemeGroupVersion.String()))
 	}
-
 	instance.Status.Versions.Reconciled = lutils.OperandVersion
-	message := "Start Semeru Compiler reconcile"
-	reqLogger.Info(message)
-	err, message = r.reconcileSemeruCompiler(instance)
-	if err != nil {
-		reqLogger.Error(err, message)
-		return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
-	}
 	reqLogger.Info("Reconcile WebSphereLibertyApplication - completed")
 	return r.ManageSuccess(common.StatusConditionTypeReconciled, instance)
 }
