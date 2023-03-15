2f0a75ba90048160798761738dee29072df486cf
Melissa Lee <halim.lee@ibm.com>
2023-03-15 09:40:53 -0700

    Make scripts executable

--- scripts/build/get-build-scripts.sh
+++ scripts/build/get-build-scripts.sh
@@ -11,4 +11,5 @@ build_scripts=($(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Accep
 for build_script in "${build_scripts[@]}"; do
     build_script_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build/${build_script}?ref=${build_script_branch}"
     curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${build_script_url}" | jq -r ".content" | base64 --decode > ${build_script_dir}/${build_script}
+    chmod +x ${build_script_dir}/${build_script}
 done
