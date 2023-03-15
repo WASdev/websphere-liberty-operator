7db294e46684cd32e960bd940fc5d13aae86a531
Melissa Lee <halim.lee@ibm.com>
2023-03-15 14:45:51 -0700

    Containerize stage and build scripts reference edit

--- scripts/build/get-build-scripts.sh
+++ scripts/build/get-build-scripts.sh
@@ -4,12 +4,12 @@ readonly build_script_dir="$(dirname "$0")"
 
 echo "Downloading build scripts"
 
-build_script_branch="build-script"
-build_script_folder_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build?ref=${build_script_branch}"
+branch="main"
+build_script_folder_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build?ref=${branch}"
 build_scripts=($(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Accept: application/vnd.github.v3+json" ${build_script_folder_url} | jq -r ".[] | .name"))
 
 for build_script in "${build_scripts[@]}"; do
-    build_script_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build/${build_script}?ref=${build_script_branch}"
+    build_script_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build/${build_script}?ref=${branch}"
     curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${build_script_url}" | jq -r ".content" | base64 --decode > ${build_script_dir}/${build_script}
     chmod +x ${build_script_dir}/${build_script}
 done
