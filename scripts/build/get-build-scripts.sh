f9f620601ff69cb5020a705bd832f984c5af825c
Melissa Lee <halim.lee@ibm.com>
2023-03-15 07:21:21 -0700

    Build script branch edit

--- scripts/build/get-build-scripts.sh
+++ scripts/build/get-build-scripts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-readonly script_dir="$(dirname "$0")"
+readonly build_script_dir="$(dirname "$0")"
 
 echo "Downloading build scripts"
 
-build_script_folder_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build?ref=build-script"
-build_scripts=($(curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Accept: application/vnd.github.v3+json" $build_script_folder_url | jq -r ".[] | .name"))
+build_script_branch="build-script"
+build_script_folder_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build?ref=${build_script_branch}"
+build_scripts=($(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Accept: application/vnd.github.v3+json" ${build_script_folder_url} | jq -r ".[] | .name"))
 
 for build_script in "${build_scripts[@]}"; do
-    # echo $build_script
-    build_script_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build/${build_script}?ref=build-script"
-    curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Accept: application/vnd.github.v3+json" "$build_script_url" | jq -r ".content" | base64 --decode > $script_dir/$build_script
+    build_script_url="https://api.github.ibm.com/repos/websphere/operators/contents/scripts/build/${build_script}?ref=${build_script_branch}"
+    curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${build_script_url}" | jq -r ".content" | base64 --decode > ${build_script_dir}/${build_script}
 done
