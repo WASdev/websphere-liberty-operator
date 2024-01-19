b63159be5594406da2344dc1fa3198ea88a12093
Leo Christy Jesuraj <leojc@ca.ibm.com>
2024-01-19 15:36:31 -0500

    Update operator version to 1.3.1
    Signed-off-by: Leo Christy Jesuraj <leojc@ca.ibm.com>

--- utils/utils.go
+++ utils/utils.go
@@ -50,7 +50,7 @@ var log = logf.Log.WithName("websphereliberty_utils")
 // Constant Values
 const serviceabilityMountPath = "/serviceability"
 const ssoEnvVarPrefix = "SEC_SSO_"
-const OperandVersion = "1.3.0"
+const OperandVersion = "1.3.1"
 const ltpaKeysMountPath = "/config/managedLTPA"
 const ltpaServerXMLOverridesMountPath = "/config/configDropins/overrides/"
 const LTPAServerXMLSuffix = "-managed-ltpa-server-xml"
