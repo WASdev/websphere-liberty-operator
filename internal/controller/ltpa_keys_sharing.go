b70a20fb7904b500e66d3be53359feecfbaf4d87
kabicin <37311900+kabicin@users.noreply.github.com>
2025-03-25 15:53:32 -0400

    Fix LTPA config leader reading wrong rotation key

--- internal/controller/ltpa_keys_sharing.go
+++ internal/controller/ltpa_keys_sharing.go
@@ -502,7 +502,7 @@ func (r *ReconcileWebSphereLiberty) generateLTPAConfig(instance *wlv1.WebSphereL
 			return ltpaXMLSecret.Name, fmt.Errorf("the internal encryption key secret does not contain field 'lastRotation'")
 		}
 
-		if encryptionKeyLastRotation, found := ltpaConfigSecret.Data["encryptionKeyLastRotation"]; found {
+		if encryptionKeyLastRotation, found := ltpaConfigSecret.Data["encryptionSecretLastRotation"]; found {
 			if string(encryptionKeyLastRotation) != string(lastRotation) {
 				err := r.DeleteResource(ltpaConfigSecret)
 				if err != nil {
@@ -540,7 +540,7 @@ func (r *ReconcileWebSphereLiberty) generateLTPAConfig(instance *wlv1.WebSphereL
 			return fmt.Errorf("failed to convert last rotation time from string to integer")
 		}
 		latestRotationTime = lastRotationTime
-		if encryptionKeyLastRotation, found := ltpaConfigSecret.Data["encryptionKeyLastRotation"]; found {
+		if encryptionKeyLastRotation, found := ltpaConfigSecret.Data["encryptionSecretLastRotation"]; found {
 			encryptionKeyLastRotationTime, err := strconv.Atoi(string(encryptionKeyLastRotation))
 			if err != nil {
 				return fmt.Errorf("failed to convert encryption key last rotation time from string to integer")
