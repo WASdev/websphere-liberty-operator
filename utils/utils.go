2f1fcc7e0a69ca8cc1304d73837a8108be98accd
kabicin <37311900+kabicin@users.noreply.github.com>
2025-10-01 11:23:22 -0400

    Reuse getOrInitProbe helper

--- utils/utils.go
+++ utils/utils.go
@@ -1115,12 +1115,8 @@ func getOrInitProbe(probe *corev1.Probe) *corev1.Probe {
 }
 
 func patchFileBasedProbe(instance *wlv1.WebSphereLibertyApplication, defaultProbe *corev1.Probe, instanceProbe *corev1.Probe, scriptName string, probeFile string) *corev1.Probe {
-	if defaultProbe == nil {
-		defaultProbe = &corev1.Probe{}
-	}
-	if instanceProbe == nil {
-		instanceProbe = &corev1.Probe{}
-	}
+	defaultProbe = getOrInitProbe(defaultProbe)
+	instanceProbe = getOrInitProbe(instanceProbe)
 	instanceProbe = rcoutils.CustomizeProbeDefaults(instanceProbe, defaultProbe)
 	if instanceProbe.Exec == nil {
 		configureFileBasedProbeExec(instance, instanceProbe, scriptName, probeFile)
